c4f2b475c5112d95b98154d76c267d6da854daf3
Pazaz <pazaz@itspazaz.com>
2025-12-10 14:01:33 -0500

    feat: Separated by-id/by-build cache routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,37 @@
 import { FastifyInstance } from 'fastify';
+import { sql } from 'kysely';
 
 import { db } from '#/db/query.js';
-import { sql } from 'kysely';
+
+async function getOnDemandFiles(cache: { id: number, game: string }) {
+    return db
+        .selectFrom('cache_ondemand')
+        .select(['archive', 'file', 'version', 'crc', 'essential'])
+        .select((eb) => [
+            eb
+                .case()
+                .when(
+                    eb.exists(
+                        eb.selectFrom('data_ondemand')
+                            .select(sql.raw('1').as('found'))
+                            .where('data_ondemand.game', '=', cache.game)
+                            .whereRef('data_ondemand.archive', '=', 'cache_ondemand.archive')
+                            .whereRef('data_ondemand.file', '=', 'cache_ondemand.file')
+                            .whereRef('data_ondemand.crc', '=', 'cache_ondemand.crc')
+                    )
+                )
+                .then(1)
+                .else(0)
+                .end()
+                .as('exists')
+        ])
+        .where('cache_id', '=', cache.id)
+        .execute();
+}
 
 export default async function (app: FastifyInstance) {
     app.get('/', async (req, reply) => {
-        const caches = await db.selectFrom('cache').selectAll().execute();
-
-        return reply.view('index', {
-            caches
-        });
+        return reply.view('index');
     });
 
     app.get('/caches', async (req, reply) => {
@@ -20,37 +42,20 @@ export default async function (app: FastifyInstance) {
         });
     });
 
-    app.get('/caches/:build', async (req: any, reply) => {
-        const { build } = req.params;
+    // get by db id
+    app.get('/caches/:id', async (req: any, reply) => {
+        const { id } = req.params;
 
-        if (build.length === 0) {
-            throw new Error('Build must be specified');
+        if (id.length === 0) {
+            throw new Error('Missing route parameters');
         }
 
-        const cache = await db.selectFrom('cache').selectAll().where('build', '=', build).executeTakeFirstOrThrow();
-        const files = await db
-            .selectFrom('cache_ondemand')
-            .select(['archive', 'file', 'version', 'crc', 'essential'])
-            .select((eb) => [
-                eb
-                    .case()
-                    .when(
-                        eb.exists(
-                            eb.selectFrom('data_ondemand')
-                                .select(sql.raw('1').as('found'))
-                                .where('data_ondemand.game', '=', cache.game)
-                                .whereRef('data_ondemand.archive', '=', 'cache_ondemand.archive')
-                                .whereRef('data_ondemand.file', '=', 'cache_ondemand.file')
-                                .whereRef('data_ondemand.crc', '=', 'cache_ondemand.crc')
-                        )
-                    )
-                    .then(1)
-                    .else(0)
-                    .end()
-                    .as('exists')
-            ])
-            .where('cache_id', '=', cache.id)
-            .execute();
+        const cache = await db
+            .selectFrom('cache')
+            .selectAll()
+            .where('id', '=', id)
+            .executeTakeFirstOrThrow();
+        const files = await getOnDemandFiles(cache);
 
         const missing = files.filter(f => !f.exists && f.essential);
         const allMissing = files.filter(f => !f.exists);
@@ -63,11 +68,29 @@ export default async function (app: FastifyInstance) {
         });
     });
 
-    app.get('/clients', async (req, reply) => {
-        return reply.view('clients');
-    });
+    // get by revision
+    app.get('/caches/:game/:build', async (req: any, reply) => {
+        const { game, build } = req.params;
 
-    app.get('/clients/:build', async (req, reply) => {
-        return reply.view('client/build');
+        if (game.length === 0 || build.length === 0) {
+            throw new Error('Missing route parameters');
+        }
+
+        const cache = await db
+            .selectFrom('cache')
+            .selectAll()
+            .where('build', '=', build)
+            .executeTakeFirstOrThrow();
+        const files = await getOnDemandFiles(cache);
+
+        const missing = files.filter(f => !f.exists && f.essential);
+        const allMissing = files.filter(f => !f.exists);
+
+        return reply.view('cache/build', {
+            cache,
+            files,
+            missing,
+            allMissing
+        });
     });
 }
