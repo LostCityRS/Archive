05f13d200cb6b45a43f0aa09d3a25d103c1fff9d
Pazaz <pazaz@itspazaz.com>
2025-12-14 23:55:38 -0500

    chore: Cache initial query in /caches routes

--- src/routes/caches/index.ts
+++ src/routes/caches/index.ts
@@ -1,13 +1,14 @@
 import { FastifyInstance } from 'fastify';
 import { Zip, ZipDeflate } from 'fflate';
-import { sql } from 'kysely';
 
-import { db } from '#/db/query.js';
+import { db, cacheExecute, cacheExecuteTakeFirst, cacheExecuteTakeFirstOrThrow } from '#/db/query.js';
+
 import FileStreamAsync from '#/io/FileStreamAsync.js';
+
 import Js5LocalDiskCacheAsync from '#/js5/Js5LocalDiskCacheAsync.js';
 
 async function getCache(id: number) {
-    return db
+    return cacheExecuteTakeFirstOrThrow(`cache_${id}`, db
         .selectFrom('cache')
         .leftJoin(
             'game',
@@ -18,99 +19,7 @@ async function getCache(id: number) {
             'cache.id', 'cache.game_id', 'game.name', 'game.display_name',
             'cache.build', 'cache.timestamp', 'cache.newspost',
             'cache.js5', 'cache.ondemand', 'cache.jag'
-        ])
-        .executeTakeFirstOrThrow();
-}
-
-async function listCacheFiles(cache: { id: number, game_id: number, js5: number, ondemand: number, jag: number }) {
-    if (cache.js5) {
-        return listCacheJs5Files(cache);
-    } else if (cache.ondemand) {
-        return listCacheOnDemandFiles(cache);
-    } else if (cache.jag) {
-        return listCacheJagFiles(cache);
-    }
-
-    return [];
-}
-
-async function listCacheJs5Files(cache: { id: number, game_id: number }) {
-    return db
-        .selectFrom('cache_js5')
-        .select(['archive', 'group', 'version', 'crc'])
-        .select(sql.raw('1').as('essential'))
-        .select((eb) => [
-            eb
-                .case()
-                .when(
-                    eb.exists(
-                        eb.selectFrom('data_js5')
-                            .select(sql.raw('1').as('found'))
-                            .where('data_js5.game_id', '=', cache.game_id)
-                            .whereRef('data_js5.archive', '=', 'cache_js5.archive')
-                            .whereRef('data_js5.group', '=', 'cache_js5.group')
-                            .whereRef('data_js5.crc', '=', 'cache_js5.crc')
-                    )
-                )
-                .then(1)
-                .else(0)
-                .end()
-                .as('exists')
-        ])
-        .where('cache_id', '=', cache.id)
-        .execute();
-}
-
-async function listCacheOnDemandFiles(cache: { id: number, game_id: number }) {
-    return db
-        .selectFrom('cache_ondemand')
-        .select(['archive', 'file', 'version', 'crc', 'essential'])
-        .select((eb) => [
-            eb
-                .case()
-                .when(
-                    eb.exists(
-                        eb.selectFrom('data_ondemand')
-                            .select(sql.raw('1').as('found'))
-                            .where('data_ondemand.game_id', '=', cache.game_id)
-                            .whereRef('data_ondemand.archive', '=', 'cache_ondemand.archive')
-                            .whereRef('data_ondemand.file', '=', 'cache_ondemand.file')
-                            .whereRef('data_ondemand.crc', '=', 'cache_ondemand.crc')
-                    )
-                )
-                .then(1)
-                .else(0)
-                .end()
-                .as('exists')
-        ])
-        .where('cache_id', '=', cache.id)
-        .execute();
-}
-
-async function listCacheJagFiles(cache: { id: number, game_id: number }) {
-    return db
-        .selectFrom('cache_jag')
-        .select(['name', 'crc'])
-        .select(sql.raw('1').as('essential'))
-        .select((eb) => [
-            eb
-                .case()
-                .when(
-                    eb.exists(
-                        eb.selectFrom('data_jag')
-                            .select(sql.raw('1').as('found'))
-                            .where('data_jag.game_id', '=', cache.game_id)
-                            .whereRef('data_jag.name', '=', 'cache_jag.name')
-                            .whereRef('data_jag.crc', '=', 'cache_jag.crc')
-                    )
-                )
-                .then(1)
-                .else(0)
-                .end()
-                .as('exists')
-        ])
-        .where('cache_id', '=', cache.id)
-        .execute();
+        ]));
 }
 
 export default async function (app: FastifyInstance) {
@@ -391,11 +300,7 @@ export default async function (app: FastifyInstance) {
             throw new Error('Missing route parameters');
         }
 
-        const cache = await db
-            .selectFrom('cache')
-            .selectAll()
-            .where('id', '=', id)
-            .executeTakeFirstOrThrow();
+        const cache = await getCache(id);
 
         const cacheData = await db
             .selectFrom('cache_js5')
@@ -427,11 +332,7 @@ export default async function (app: FastifyInstance) {
             throw new Error('Missing route parameters');
         }
 
-        const cache = await db
-            .selectFrom('cache')
-            .selectAll()
-            .where('id', '=', id)
-            .executeTakeFirstOrThrow();
+        const cache = await getCache(id);
 
         const cacheData = await db
             .selectFrom('cache_ondemand')
@@ -463,11 +364,7 @@ export default async function (app: FastifyInstance) {
             throw new Error('Missing route parameters');
         }
 
-        const cache = await db
-            .selectFrom('cache')
-            .selectAll()
-            .where('id', '=', id)
-            .executeTakeFirstOrThrow();
+        const cache = await getCache(id);
 
         const cacheData = await db
             .selectFrom('cache_jag')
