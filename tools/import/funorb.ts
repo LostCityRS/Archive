c11bc7a43140f8f55ced2ec8e38d65d92d52b21f
Pazaz <pazaz@itspazaz.com>
2025-12-15 23:14:52 -0500

    feat: Updated funorb script

--- tools/import/funorb.ts
+++ tools/import/funorb.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import { db } from '#/db/query.js';
+
 import { importJs5WithoutIndex } from '#tools/import/util.js';
 
 const args = process.argv.slice(2);
@@ -10,7 +13,19 @@ if (args.length < 3) {
 try {
     const [source, game, build] = args;
 
-    await importJs5WithoutIndex(source, game, build);
+    console.log(`Importing from ${source} as ${game} ${build}`);
+    const cache = await importJs5WithoutIndex(source, game, build);
+    console.log(`https://archive2.lostcity.rs/caches/${cache.id}`);
+
+    await db
+        .insertInto('cache_source')
+        .values({
+            cache_id: cache.id,
+            timestamp: new Date(),
+            attribution: 'OpenRS2',
+            description: source
+        })
+        .execute();
 } catch (err) {
     if (err instanceof Error) {
         console.log(err.message);
