a4de10472a67aaabbc20f088db0c44944fa3ea87
BigShot <kenjohnson1994@gmail.com>
2025-12-16 20:24:03 -0600

    fix: Cache list breadcrumb link

--- src/routes/caches/index.ts
+++ src/routes/caches/index.ts
@@ -134,7 +134,7 @@ export default async function (app: FastifyInstance) {
             title: `${cache.display_name} ${cache.build} Cache`,
             breadcrumbs: [
                 { label: 'Caches', href: '/' },
-                { label: `${cache.display_name} Caches`, href: `/list/${cache.name}` }
+                { label: `${cache.display_name} Caches`, href: `/caches/list/${cache.name}` }
             ]
         });
     });
